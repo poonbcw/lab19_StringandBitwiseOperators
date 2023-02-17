dbe730eaf9b4ed61de2a3518c37d7076eee3d5b3
poon <poon15112@gmail.com>
2023-02-17 17:56:42 +0700

    lap19_1 completed

--- lab19_1.cpp
+++ lab19_1.cpp
@@ -3,7 +3,7 @@
 #include<vector>
 #include<string>
 #include<cstdlib>
-
+#include<cctype>
 using namespace std;
 
 char score2grade(int score){
@@ -20,20 +20,59 @@ string toUpperStr(string x){
     return y;
 }
 
-void importDataFromFile(){
-
+void importDataFromFile(string fn, vector<string> &name,vector<int> &score, vector<char> &grade){
+    ifstream source(fn);
+    string s;
+    while(getline(source,s)){
+        
+        char format[] = "%[^:]: %d %d %d";
+        char n[100];
+        int a, b, c;
+        sscanf(s.c_str(), format, n, &a, &b, &c);
+        name.push_back(n);
+        score.push_back(a+b+c);
+        grade.push_back(score2grade(a+b+c));
+    }
 }
 
-void getCommand(){
-
+void getCommand(string &command, string &key){
+    cout << "Please input your command: ";
+    cin >> command;
+    if(toUpperStr(command) == "NAME" || toUpperStr(command) == "GRADE"){
+        cin.ignore();
+        getline(cin,key);
+    }
 }
 
-void searchName(){
+void searchName(vector<string> name, vector<int> score, vector<char> grade, string key){
+    cout << "---------------------------------\n";
+    int x = 0;
+    for(unsigned int i = 0; i < name.size(); i++){
+        if(key == toUpperStr(name[i])){
+            cout << name[i] << "'s score = " << score[i] << "\n";
+            cout << name[i] << "'s grade = " << grade[i] << "\n";
+            x++;
+        }else if(i == name.size()-1 && x == 0){
+            cout << "Cannot found." << "\n";
+        }
 
+    }
+    cout << "---------------------------------\n";
 }
 
-void searchGrade(){
-
+void searchGrade(vector<string> name, vector<int> score, vector<char> grade, string key){
+    cout << "---------------------------------\n";
+    int x = 0;
+    for(unsigned int i = 0; i < name.size(); i++){
+        
+        if(*key.c_str() == grade[i]){
+            cout << name[i] << " " << "(" << score[i] << ")" << "\n";
+            x++;
+        }else if(i == name.size()-1 && x == 0){
+            cout << "Cannot found." << "\n";
+        }
+    }
+    cout << "---------------------------------\n";
 }
 
 
